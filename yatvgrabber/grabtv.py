b032cd27b1d9ec3249fde5242cf99b71d7a87fa8
lars.schmohl@gmail.com <lars.schmohl@gmail.com@1b0409f8-bb2b-11dd-a80a-757f205f25d6>
2012-03-20 22:52:14 +0000

    update
    git-svn-id: https://yatvgrabber.googlecode.com/svn/trunk@156 1b0409f8-bb2b-11dd-a80a-757f205f25d6

--- yatvgrabber/grabtv.py
+++ yatvgrabber/grabtv.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 
-import argparse
-import sys
 import os
 import re
+import sys
 import urllib
-from configobj import ConfigObj
+#import logging
+import argparse
+import subprocess
 from random import choice
+from configobj import ConfigObj
 
 def main():
     # argument parsing
@@ -14,7 +16,10 @@ def main():
     
     # override the urllib useragent - to get the custom user agent
     urllib._urlopener = AppOpener()
-
+    
+    # run the pre grab step
+    preGrabCleanUp()
+    
     # read / set the grabber configuration file
     grabConf = ConfigObj(ArgumentParser.args.configfile, raise_errors=True)
     if not os.path.isfile(ArgumentParser.args.configfile):
@@ -37,11 +42,18 @@ def main():
     for line in open(ArgumentParser.args.channelfile, "r"):
         temp = line.split('#')
         DataStorage.channelList[temp[0]] = temp[1]
+    # get the program data
     # looping for the days
     for dayIndex in range(0, ArgumentParser.args.days+1):
-        week = dayIndex//7
-        day = dayIndex - week*7
-        parseChannelData(grabConf['page'][0], week, day)
+        parseChannelData(grabConf['page'][0], dayIndex//7, dayIndex%7)
+    
+    # export the program data to xmltv file
+    # debug output is here
+    for program in DataStorage.programData.keys():
+        print str(program)+': '+str(DataStorage.programData[program])
+    
+    # post grab cleanup
+    postGrabCleanUp()
 
 class ArgumentParser():
     @staticmethod
@@ -61,37 +73,56 @@ class ArgumentParser():
                             default="/etc/yatvgrabber/channel.grab", help='channel file for the grabber')
         parser.add_argument('--cachedir', type=str, default="/var/cache/yatvgrabber",
                             help='cache directory for the grabber')
-        parser.add_argument('--update-only', action="store_true", default=False,
-                            help='only generate an update file (only adds new downloaded programs)')
         parser.add_argument('--local', action="store_true", default=False,
                             help='process only the local stored cache files')
-        parser.add_argument('--disable-cleanup', action="store_true", default=False,
-                            help='do no cleanup after parsing')
-        parser.add_argument('--verbose', type=int, default=0,
-                            help='make it verbose')
+        #parser.add_argument('--verbose', type=int, default=0,
+        #                    help='make it verbose')
         
         ## parse the arguments
         ArgumentParser.args = parser.parse_args()
+        ArgumentParser.args.outputfile = os.path.realpath(ArgumentParser.args.outputfile)
+        ArgumentParser.args.configfile = os.path.realpath(ArgumentParser.args.configfile)
+        ArgumentParser.args.channelfile = os.path.realpath(ArgumentParser.args.channelfile)
+        ArgumentParser.args.cachedir = os.path.realpath(ArgumentParser.args.cachedir)
         
         # verbose - print the arguments
-        if ArgumentParser.args.verbose > 0:
-            print ArgumentParser.args
+        # somebody make an good advise for logging
+        #logging.disable(logging.CRITICAL + 1 - (ArgumentParser.args.verbose * 10))
+
+def preGrabCleanUp():
+    # create the config dir if needed
+    if not os.path.isfile(ArgumentParser.args.configfile):
+        configdir = os.path.dirname(ArgumentParser.args.configfile)
+        if not os.path.isdir(configdir) and configdir != '':
+            os.makedirs(configdir)
+    # create the channel dir if needed
+    if not os.path.isfile(ArgumentParser.args.channelfile):
+        channeldir = os.path.dirname(ArgumentParser.args.channelfile)
+        if not os.path.isdir(channeldir) and channeldir != '':
+            os.makedirs(channeldir)
+    
+    if ArgumentParser.args.cachedir != '' and not os.path.isdir(ArgumentParser.args.cachedir):
+        # create the cache dir if needed
+        os.makedirs(ArgumentParser.args.cachedir)
+    else:
+        # cleanup the grabbed files - just the empty files
+        subprocess.call('find '+ ArgumentParser.args.cachedir +' -type f -empty -exec rm -f \'{}\' +', shell=True)
+
+def postGrabCleanUp():
+    # cleanup the grabbed files - just the empty files
+    subprocess.call('find '+ ArgumentParser.args.cachedir +' -type f -empty -exec rm -f \'{}\' +', shell=True)
+    # cleanup the grabbed files - files which are not used anymore
+    subprocess.call('find '+ ArgumentParser.args.cachedir +' -type f -atime +1 -exec rm -f \'{}\' +', shell=True)
 
 class DataStorage():
     channelList = dict()
-    channelData = dict()
+    programData = dict()
 
 class AppOpener(urllib.FancyURLopener):
-    user_agents = ['Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)',
-               'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)',
-               'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Sindup)',
-               'Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
-               'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6',
-               'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-               'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
-               'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98; DigExt)',
-               'Mozilla/4.0 (compatible; MSIE 6.0; Windows 98; Win 9x 4.90)',
-               'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0']
+    user_agents = ['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)',
+                   'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2',
+                   'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0']
     version = choice(user_agents)
 
 class Parser():
@@ -123,6 +154,8 @@ class Parser():
         if not ArgumentParser.args.local:
             # always retrieve the day page in none local mode
             urllib.urlretrieve(base_url + "/tvtv/index.vm?weekId="+str(week)+"&dayId="+str(day)+"&chnl="+str(channelId), filename)
+            # use channelWeek to get the hole week for one channel
+            #urllib.urlretrieve(base_url + "/tvtv/index.vm?weekId="+str(week)+"&dayId="+str(day)+"&channelWeek="+str(channelId), filename)
         if  not os.path.isfile(filename):
             print sys.stderr, "unable to find file " +filename
             return str()
@@ -138,6 +171,7 @@ class Parser():
         if  not os.path.isfile(filename):
             print sys.stderr, "unable to find file " +filename
             return str()
+        os.utime(filename, None)    ## "touch" the file
         return open(filename, 'r').read()
 
 def parseChannelList(pagename):
@@ -158,10 +192,29 @@ def parseChannelList(pagename):
 
 def parseChannelData(pagename, week, day):
     regExProgramId = re.compile(r'programmeId=([0-9]+)')
+    regExChannelId = re.compile(r's.prop16="[^\(]+\(([0-9]+)\)"')
+    regExTitle = re.compile(r's.prop5="(.*)\[[0-9]+\]"')
     for channelId in DataStorage.channelList.keys():
         for dayLine in Parser.getDayPage(pagename, week, day, channelId).split('\n'):
             for programId in regExProgramId.findall(dayLine):
-                for programLine in Parser.getProgramPage(pagename, programId):
+                # create the progam data container
+                if not DataStorage.programData.has_key(programId):
+                    DataStorage.programData[programId] = dict()
+                programPage = Parser.getProgramPage(pagename, programId)
+                
+                # get the channel id from the page
+                for foundChanId in regExChannelId.findall(programPage):
+                    DataStorage.programData[programId]['channelid'] = foundChanId
+                # get the title from the page
+                for foundTitle in regExTitle.findall(programPage):
+                    DataStorage.programData[programId]['title'] = foundTitle.strip()
+                
+                #cut down the content
+                programPage = programPage.split(r'class="program-content"')[1]
+                programPage = programPage.split(r'class="list_detail"')[0]
+                
+                # parse the page content
+                for programLine in programPage.split('\n'):
                     pass
 
 # open the gui
