9c0989fc3c1a7d125224f2b1007d1723b02b8fa1
lars.schmohl@gmail.com <lars.schmohl@gmail.com@1b0409f8-bb2b-11dd-a80a-757f205f25d6>
2015-03-04 20:41:21 +0000

    mc2xml: update grab script
    git-svn-id: https://yatvgrabber.googlecode.com/svn/trunk@212 1b0409f8-bb2b-11dd-a80a-757f205f25d6

--- mc2xml_helper/grab-mc2xml.sh
+++ mc2xml_helper/grab-mc2xml.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-set -e
-
 XMLTV_FILE="/tmp/mc2xml.xml"
 LOG_FILE="/tmp/grab_mc2xml.log"
 SOURCE_IDS="1"
 
+rm -f $LOG_FILE
 touch $LOG_FILE
 chmod 666 $LOG_FILE
 date >> $LOG_FILE
-
-#remove the old file
-if [ -e $XMLTV_FILE ]; then
-    mv -f $XMLTV_FILE ${XMLTV_FILE}.old >> $LOG_FILE
-fi
 
 cd /var/cache/mc2xml
 
@@ -21,30 +15,29 @@ cd /var/cache/mc2xml
 echo "-----------" >> $LOG_FILE
 echo "uk - London" >> $LOG_FILE
 echo "-----------" >> $LOG_FILE
-mc2xml -c gb -g "SW1X 7LA" -D mc2xml_gb.dat -o /tmp/xmltv_gb.xml >> $LOG_FILE
+[ -e $XMLTV_FILE ] && rm -f $XMLTV_FILE  >> $LOG_FILE
+mc2xml -U -c gb -g "SW1X 7LA" -D mc2xml_gb.dat -o $XMLTV_FILE >> $LOG_FILE
+[ $? -eq 0 ] && nice mythfilldatabase --only-update-guide --file --sourceid $id --xmlfile $XMLTV_FILE >> $LOG_FILE
+
 
 echo "---------" >> $LOG_FILE
 echo "au - Wien" >> $LOG_FILE
 echo "---------" >> $LOG_FILE
-mc2xml -c at -g 1010 -D mc2xml_au.dat -I /tmp/xmltv_gb.xml -o /tmp/xmltv_au.xml >> $LOG_FILE
+[ -e $XMLTV_FILE ] && rm -f $XMLTV_FILE  >> $LOG_FILE
+mc2xml -U -c at -g 1010 -D mc2xml_au.dat -o $XMLTV_FILE >> $LOG_FILE
+[ $? -eq 0 ] && nice mythfilldatabase --only-update-guide --file --sourceid $id --xmlfile $XMLTV_FILE >> $LOG_FILE
 
 echo "---------" >> $LOG_FILE
 echo "de - Berlin Mitte" >> $LOG_FILE
 echo "---------" >> $LOG_FILE
-mc2xml -c de -g 10115 -D mc2xml_de.dat -I /tmp/xmltv_au.xml -o /tmp/xmltv_de.xml >> $LOG_FILE
+[ -e $XMLTV_FILE ] && rm -f $XMLTV_FILE  >> $LOG_FILE
+mc2xml -U -c de -g 10115 -D mc2xml_de.dat -o $XMLTV_FILE >> $LOG_FILE
+[ $? -eq 0 ] && nice mythfilldatabase --only-update-guide --file --sourceid $id --xmlfile $XMLTV_FILE >> $LOG_FILE
 
 echo "---------" >> $LOG_FILE
 echo "ch - Ballwil" >> $LOG_FILE
 echo "---------" >> $LOG_FILE
-mc2xml -c ch -g 6275 -D mc2xml_ch.dat -I /tmp/xmltv_de.xml -o $XMLTV_FILE >> $LOG_FILE
-
-# add the data to the db
-if [ -e $XMLTV_FILE ]; then
-    for id in $SOURCE_IDS
-    do
-        nice mythfilldatabase --only-update-guide --file --sourceid $id --xmlfile $XMLTV_FILE >> $LOG_FILE
-    done
-else
-    echo "no xmltv file found - $XMLTV_FILE" >> $LOG_FILE
-fi
+[ -e $XMLTV_FILE ] && rm -f $XMLTV_FILE  >> $LOG_FILE
+mc2xml -U -c ch -g 6275 -D mc2xml_ch.dat -o $XMLTV_FILE >> $LOG_FILE
+[ $? -eq 0 ] && nice mythfilldatabase --only-update-guide --file --sourceid $id --xmlfile $XMLTV_FILE >> $LOG_FILE
 
